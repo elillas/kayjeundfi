38270bbacb3a1107a70db0653260612129d1e689
Elimane Dieng <elimane.dieng@example.com>
2025-06-07 18:55:35 +0000

    Add HomepageSoftwareForm and BlogAndReviewForm to fix admin import error

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,6 +1,17 @@
 from django import forms
+from .models import HomepageSoftware, BlogAndReview
 
 class ContactForm(forms.Form):
     full_name = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
+
+class HomepageSoftwareForm(forms.ModelForm):
+    class Meta:
+        model = HomepageSoftware
+        fields = '__all__'
+
+class BlogAndReviewForm(forms.ModelForm):
+    class Meta:
+        model = BlogAndReview
+        fields = '__all__'
